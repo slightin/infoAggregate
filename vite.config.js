d14cda7324db1490da4fe00a490f9fced7189574
slightin <silentmiss@outlook.com>
2023-04-22 23:30:40 +0800

    add el-icon

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,38 @@
-import {
-    fileURLToPath,
-    URL
-} from 'node:url'
-
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(),
+    plugins: [
+        vue(),
         AutoImport({
-            resolvers: [ElementPlusResolver()],
+            // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+            imports: ['vue'],
+            // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    prefix: 'Icon',
+                }),
+            ],
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                }),
+            ],
+        }),
+        Icons({
+            compiler: 'vue3',
+            autoInstall: true,
         }),
     ],
     resolve: {
