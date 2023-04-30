30a73e6aae3c76f2f7f3c2c044232fd09feb8145
slightin <silentmiss@outlook.com>
2023-04-30 17:55:45 +0800

    :lipstick: iconfont

--- src/views/HotNewsView.vue
+++ src/views/HotNewsView.vue
@@ -2,18 +2,18 @@
     <el-container>
         <el-aside width="200px">
             <el-menu default-active="1" background-color="#4A525A" text-color="#ffffff">
-                <el-menu-item index="1" @click="gethot('weibo')">微博热搜</el-menu-item>
-                <el-menu-item index="2" @click="gethot('zhihu')">知乎热榜</el-menu-item>
+                <el-menu-item index="1" @click="gethot('weibo')"><i class="fa-brands fa-weibo"></i>微博热搜</el-menu-item>
+                <el-menu-item index="2" @click="gethot('zhihu')"><i class="iconfont icon-zhihu"></i>知乎热榜</el-menu-item>
             </el-menu>
         </el-aside>
         <el-main>
-            <el-scrollbar height='calc(100vh - 78.4px)'>
+            <el-scrollbar height='calc(100vh - 90px)'>
                 <a v-for='item in hotlist' :href='item.link'>
                     <el-card class='icard'>
                     <el-link>{{item.title}}</el-link>
                     </el-card>
                 </a>
-                <el-backtop right="50" bottom="50" target=".el-scrollbar__wrap"/>
+                <el-backtop :right="50" :bottom="50" target=".el-scrollbar__wrap"/>
             </el-scrollbar>
         </el-main>
     </el-container>
@@ -43,5 +43,8 @@
     }
     .el-main{
         padding: 10px 20px 10px 20px;
+    }
+    .fa-brands, .iconfont{
+        padding: 0 10px 0 10px;
     }
 </style>
